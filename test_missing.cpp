038af63f870017d73a8da89a9ff9a408d549cad8
JItaimei123 <b0124gsa@gmail.com>
2026-07-06 20:21:48 +0800

    📝 新增：0～n-1中缺失的数字（剑指 Offer 53-II）（左/右边界查找双版本）

---
+++ test_missing.cpp
@@ -0,0 +1,32 @@
+﻿#include <vector>
+#include <iostream>
+using namespace std;
+
+int missingNumber(vector<int>& nums) {
+    int left = 0, right = nums.size();
+    while (left < right) {
+        int mid = left + (right - left + 1) / 2;  // 右中点
+        if (nums[mid] == mid) { // 左边没有缺失，去右边找
+            left = mid;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return left;
+}
+
+int main() {
+    vector<int> test1 = {0,1,3};
+    cout << "Test [0,1,3]: " << missingNumber(test1) << " (expected 2)" << endl;
+    
+    vector<int> test2 = {0,1,2,3,4,5,6,7,9};
+    cout << "Test [0,1,2,3,4,5,6,7,9]: " << missingNumber(test2) << " (expected 8)" << endl;
+    
+    vector<int> test3 = {0,1,2,3};
+    cout << "Test [0,1,2,3]: " << missingNumber(test3) << " (expected 4)" << endl;
+    
+    vector<int> test4 = {1,2,3};
+    cout << "Test [1,2,3]: " << missingNumber(test4) << " (expected 0)" << endl;
+    
+    return 0;
+}
